c8c02d07b13cf31e9de7d046311ef52febcb67a9
niuyiqun <294921990@qq.com>
2021-03-11 19:54:31 +0800

    增加了swagger2模块，访问swagger-ui查看当前所有controller，aop切面类删除，与Bean的自动装配产生了冲突（暂时没搞明白原因所在）

--- src/main/java/com/example/demo/Test0Application.java
+++ src/main/java/com/example/demo/Test0Application.java
@@ -5,11 +5,12 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.boot.web.servlet.ServletComponentScan;
 import org.springframework.context.annotation.ComponentScan;
-
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 
 @ServletComponentScan
 @SpringBootApplication
+@EnableSwagger2
 public class Test0Application {
 
 	public static void main(String[] args) {
